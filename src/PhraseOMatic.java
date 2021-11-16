c679dd2fe28dfe2ed74d946e659c5daed8ebfc1b
Evgeniy Timofeev <tima.exe@gmail.com>
2021-11-16 17:53:20 +0300

    генератор случайных фраз

--- src/PhraseOMatic.java
+++ src/PhraseOMatic.java
@@ -1,7 +1,18 @@
 public class PhraseOMatic {
     public static void main(String[] args) {
-        String[] wordListOne = {"круглосуточный", "разноторонний", "легкообучаемый", "труднопроизносимый", "вспыльчивый","развратный","обдолбленный","тупой"};
-        String[] wordListTwo = {"женственный", "голубой", "мужественный", "сильный", "слабый","посредственный","класический","консервативный"};
+        String[] wordListOne = {"круглосуточный", "разносторонний", "легкообучаемый", "труднопроизносимый", "вспыльчивый","развратный","обдолбленный","тупой"};
+        String[] wordListTwo = {"женственный", "голубой", "мужественный", "сильный", "слабый","посредственный","классический","консервативный"};
         String[] wordListThree = {"дурак", "хомяк", "простак", "чудак", "властелин","нищеброд","слесарь","Игорь"};
+
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
+
+        int rand1 = (int) (Math.random() * oneLength);
+        int rand2 = (int) (Math.random() * twoLength);
+        int rand3 = (int) (Math.random() * threeLength);
+
+        String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
+        System.out.println("Все, что нам нужно, - это " + phrase);
     }
 }
