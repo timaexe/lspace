f6e9b388459e3f0ecf4ffa016c05043525ef9eba
Evgeniy Timofeev <tima.exe@gmail.com>
2021-12-11 22:18:38 +0300

    для Максима

---
+++ src/task0102/Solution.java
@@ -0,0 +1,15 @@
+package task0102;
+
+/*
+Комплимент учителю
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println ("Элли самая красивая");
+        System.out.println ("Элли самая красивая");
+        System.out.println ("Элли самая красивая");
+        System.out.println ("Элли самая красивая");
+        System.out.println ("Элли самая красивая");//напишите тут ваш код
+    }
+}
