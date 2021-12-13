6f837428c33089ca7e97231e6f61dd1cf8bfae95
Evgeniy Timofeev <tima.exe@gmail.com>
2021-12-14 00:37:31 +0300

    много простых заданий для набивания руки. делов на 20 минут

---
+++ src/task0110/Solution.java
@@ -0,0 +1,12 @@
+package task0110;
+
+/*
+На нашем экране — переменная
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String name = "текст-текст-текст";
+        System.out.println(name);
+    }
+}
---
+++ src/task0140/Solution.java
@@ -0,0 +1,14 @@
+package task0140;
+
+/*
+Выводим квадрат числа
+*/
+
+public class Solution {
+    public static int number = 25;
+
+    public static void main(String[] args) {
+        int square = number*number;
+        System.out.println(square);
+    }
+}
