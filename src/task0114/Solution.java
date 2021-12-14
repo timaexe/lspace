4757a27c8ebe261219c2803ae27b6711aa280815
Evgeniy Timofeev <tima.exe@gmail.com>
2021-12-14 22:33:56 +0300

    Становится интересней

---
+++ src/task0114/Solution.java
@@ -0,0 +1,21 @@
+package task0114;
+
+/*
+JavaRush. Learn once - use anywhere
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String a = "JavaRush. Learn once - use anywhere";
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+    }
+}
