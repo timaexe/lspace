143fb01bd4849815b1a54077378fb6b09be45d1b
Evgeniy Timofeev <tima.exe@gmail.com>
2021-12-21 00:56:22 +0300

    У меня подозрение, что остальные эти два уровня прошли за пару часов. Плохих программистов не берем. Там в комментах такую фигню спрашивают, что явно джунами без мегаусилий таким не стать. Но я таки тоже тормоз

---
+++ src/task0126/Solution.java
@@ -0,0 +1,27 @@
+package task0126;
+
+/*
+Не хочешь  - решай нехотя
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String a = "Хочешь - решай задачи, не хочешь - решай нехотя";
+        System.out.println (a);
+        System.out.println (a);
+        System.out.println (a);
+        System.out.println (a);
+        System.out.println (a);
+        System.out.println (a);
+        System.out.println (a);
+        System.out.println (a);
+        System.out.println (a);
+        System.out.println (a);
+        System.out.println (a);
+        System.out.println (a);
+        System.out.println (a);
+        System.out.println (a);
+        System.out.println (a);
+        System.out.println (a);
+    }
+}
