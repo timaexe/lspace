2ccc724c4e28e28ec035b385adf824a0b1b1c7b7
Evgeniy Timofeev <tima.exe@gmail.com>
2022-01-09 21:48:44 +0300

    фантазии нет на коммиты. Очередные задачи

---
+++ src/JR/task0129/Solution.java
@@ -0,0 +1,17 @@
+package JR.task0129;
+
+/*
+Считаем длину окружности
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        printCircleLength(5);
+    }
+
+    public static void printCircleLength(int radius) {
+        double pi = 3.14;
+        double length = 2*pi*radius;
+        System.out.println(length);
+    }
+}
---
+++ src/JR/task0207/Solution.java
@@ -0,0 +1,13 @@
+package JR.task0207;
+
+/*
+Кусочек калькулятора
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 3, b = 5;
+        System.out.println (a+b);
+        System.out.println (a*b);
+    }
+}
