0e841fc77a556108007fddb1d7cc458a8b9bcf75
Evgeniy Timofeev <tima.exe@gmail.com>
2022-01-17 23:40:58 +0300

    следующий коммит я просто ударю по клавишам

---
+++ src/JR/task0208/Solution.java
@@ -0,0 +1,16 @@
+package JR.task0208;
+
+/*
+Одного кота нам мало
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat petr = new Cat();
+        Cat nikita = new Cat();
+    }
+
+    public static class Cat {
+
+    }
+}
